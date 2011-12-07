0682489cfd62210dc84b030a120c95009c49ae50
Blake Dickie <bdickie@landora.net>
2011-12-07 07:00:22 +0000

    Initial start of non Netbeans VideoBrowser.

---
+++ GSVideoManagerApp/src/main/java/net/landora/gsvideomanagerapp/App.java
@@ -0,0 +1,15 @@
+package net.landora.gsvideomanagerapp;
+
+import net.landora.videomanager.VideoManagerApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        VideoManagerApp.launch(VideoManagerApp.class, args);
+    }
+}
